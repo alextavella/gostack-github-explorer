7ed7b14074792e7168256c714435a3dab3ea6772
alextavella <alex.tavella-ext@viavarejo.com.br>
2020-06-25 16:31:25 -0300

    style: 💄 Update title on dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -80,7 +80,7 @@ const Dashboard: React.FC = () => {
   return (
     <>
       <img src={logo} alt="Github Explorer" />
-      <Title>Explorer respositórios no Github</Title>
+      <Title>Explore repositórios no Github</Title>
 
       <Form hasError={!!inputError} onSubmit={handleAddRepository}>
         <input
