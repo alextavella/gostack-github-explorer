4fd6d38a1ddd3831993f488852262f74063d7255
alextavella <alex.tavella-ext@viavarejo.com.br>
2020-06-25 15:48:45 -0300

    feat: 🎸 Impl dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,53 +1,112 @@
-import React from 'react';
-import logo from '../../assets/logo.svg';
-import { Title, Form, Repositories } from './styles';
+import React, { useEffect, useState } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import logo from '../../assets/logo.svg';
+import api from '../../services/api';
+import { Error, Form, Repositories, Title } from './styles';
+
+interface Repository {
+  name?: string;
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
+const LOCALSTORAGE_KEY = '@GithubExplore:repositories';
+const NEW_REPO_SUGGESTION = 'alextavella/gostack-typeorm-upload';
 
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState<string>(NEW_REPO_SUGGESTION);
+
+  const [inputError, setInputError] = useState<string>('');
+
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const repos = localStorage.getItem(LOCALSTORAGE_KEY);
+    return repos ? JSON.parse(repos) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(repositories));
+  }, [repositories]);
+
+  async function handleAddRepository(
+    e: React.FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    e.preventDefault();
+
+    if (!newRepo) {
+      setInputError('Digite o autor/nome do repositório');
+      return;
+    }
+
+    const repo = repositories.find((r) => r.name === newRepo);
+    if (repo) {
+      const repoIndex = repositories.findIndex((r) => r.name === newRepo);
+      repositories.splice(repoIndex, 1);
+
+      setRepositories([repo, ...repositories]);
+
+      setInputError('');
+      setNewRepo('');
+      return;
+    }
+
+    try {
+      const response = await api.get<Repository>(`/repos/${newRepo}`);
+      const {
+        full_name,
+        description,
+        owner: { login, avatar_url },
+      } = response.data;
+
+      const repository = {
+        name: newRepo,
+        full_name,
+        description,
+        owner: { login, avatar_url },
+      };
+
+      setRepositories([repository, ...repositories]);
+      setInputError('');
+    } catch (e) {
+      setInputError('Error ao pesquisar repositórios');
+    } finally {
+      setNewRepo('');
+    }
+  }
+
   return (
     <>
       <img src={logo} alt="Github Explorer" />
       <Title>Explorer respositórios no Github</Title>
 
-      <Form>
-        <input placeholder="Digite o nome do repositório" />
-        <button type="button">Pesquisar</button>
+      <Form hasError={!!inputError} onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
+        />
+        <button type="submit">Pesquisar</button>
       </Form>
 
+      {inputError && <Error>{inputError}</Error>}
+
       <Repositories>
-        <a href="teste">
-          <img
-            src="https://avatars3.githubusercontent.com/u/3128408?s=460&u=ca5efe074f71f07d0d08f668f66589610b8df018&v=4"
-            alt=""
-          />
-          <div>
-            <strong>gostack-typeorm-upload</strong>
-            <p>Desafio sobre Typeorm aplicado no Bootcamp GoStack</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars3.githubusercontent.com/u/3128408?s=460&u=ca5efe074f71f07d0d08f668f66589610b8df018&v=4"
-            alt=""
-          />
-          <div>
-            <strong>gostack-typeorm-upload</strong>
-            <p>Desafio sobre Typeorm aplicado no Bootcamp GoStack</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars3.githubusercontent.com/u/3128408?s=460&u=ca5efe074f71f07d0d08f668f66589610b8df018&v=4"
-            alt=""
-          />
-          <div>
-            <strong>gostack-typeorm-upload</strong>
-            <p>Desafio sobre Typeorm aplicado no Bootcamp GoStack</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
+        {repositories.map((repository) => (
+          <a key={repository.full_name} href="teste">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
